eb92997c49815420e27a8ba4b6c5de05da8530ce
Arvid Larsson <arvla626@student.liu.se>
2019-11-12 08:41:43 +0100

    Update build_set

--- src/build_set.py
+++ src/build_set.py
@@ -1,23 +1,27 @@
 from episode import Episode
+from functools import lru_cache
 import regex as re
 
-with open('./all_episodes.txt') as f:
+with open("./all_episodes.txt") as f:
     all_episodes = f.read().splitlines()
 
-print(len(all_episodes))
-def episodes_with_name(name):
+names = ["Sam Harris", "Jordan Petersson", "Nick Bostrom", "Hillary Clinton"]
+
+def episodes_with_names(names):
+    """
+    Takes a list of names and returns a list of episodes where the names occur in 
+    the episode description.
+    """
     episodes = []
-    for title, description  in zip(all_episodes[0::2], all_episodes[1::2]):
-        name_in_title = re.findall(name, title)
-        name_in_description = re.findall(name, description)
-        if name_in_description:
-            title = re.sub(name, 'NAME', title)
-            description = re.sub(name, 'NAME', description)
-            ep = Episode(title, description)
-            episodes.append(ep)
+    for name in names:
+        for title, description in zip(all_episodes[0::2], all_episodes[1::2]):
+            name_in_title = re.findall(name, title)
+            name_in_description = re.findall(name, description)
+            if name_in_description:
+                title = re.sub(name, "NAME", title)
+                description = re.sub(name, "NAME", description)
+                ep = Episode(title, description)
+                episodes.append(ep)
     return episodes
 
-episodes = episodes_with_name("Sam Harris")
-for ep in episodes:
-    print(" ".join(ep.window()))
 
