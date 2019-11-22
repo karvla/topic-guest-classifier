8ca2142f3c9d084a09f1756f99c83ee7f27f46c7
Arvid Larsson <arvla626@student.liu.se>
2019-11-22 11:09:42 +0100

    Fix split

--- src/split_set.py
+++ src/split_set.py
@@ -1,18 +1,32 @@
 import sys
+import random
 
 labeled_set_name = sys.argv[1]
 with open(labeled_set_name) as f:
     labed_lines = f.readlines()
 
 n_samples = len(labed_lines)/4
-n_cut = round(n_samples * 0.8)
+n_cut = round(n_samples * 0.8) # 80% 
 
-labed_lines_train = labed_lines[:n_cut*4]
-labed_lines_validate = labed_lines[n_cut*4:]
+labled_lines_validate = labed_lines[n_cut*4:]
+labled_lines_train = labed_lines[:n_cut*4]
+test_lines = [4*random.randrange(n_cut) for n in range(200)]
+
+test_set = []
+for n in test_lines:
+    test_set.append(labled_lines_train.pop(n))
+    test_set.append(labled_lines_train.pop(n))
+    test_set.append(labled_lines_train.pop(n))
+    test_set.append(labled_lines_train.pop(n))
 
 with open(labeled_set_name[:-4] + "_train.txt", "w") as f:
-    f.writelines(labed_lines_train)
+    f.writelines(labled_lines_train)
 
 with open(labeled_set_name[:-4] + "_validate.txt", "w") as f:
-    f.writelines(labed_lines_validate)
+    f.writelines(labled_lines_validate)
+
+with open("test_set.txt", "w") as f:
+    f.writelines(test_set)
+
+
 
