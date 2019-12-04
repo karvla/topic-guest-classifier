4857b55ae888c9753744ad492c84e6c28b2d0702
Arvid Larsson <arvla626@student.liu.se>
2019-12-04 16:44:04 +0100

    Removed plotting

--- src/autotagger_4.py
+++ src/autotagger_4.py
@@ -2,24 +2,54 @@ import pickle
 import trie
 import sys
 from episode import Episode
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pickle
+
 
 """ 
 Tags data but assuming that popular name-mentions will mostly be topics while
 not so popular will be guests.
 """
-def plot_names_histogram(names, title=""):
-    n_per_name = list(map(len, names.values()))
-    plt.hist(n_per_name, bins=1000, log=True, range=(1, 1200))
-    plt.ylabel("Number of names")
-    plt.xlabel("Number of mentions")
-    plt.title(title)
-    plt.savefig("./plots/"+title+ ".png")
+def plot_names_histogram(names, limits=(20, 400)):
 
-def label_set():
-    """
-    Lables the the set.
-    """
+    n_per_name = list(map(len, names.values()))
+
+    plt.figure(figsize=(5,4))
+    N, bins, patches = plt.hist(n_per_name, bins=60, log=True, range=(0, 600), color='grey')
+    colors = ['#EA5739','#6de581']
+
+    print(len(patches))
+    for i in range(0, 2):
+        patches[i].set_color(colors[1])
+        patches[i].set_edgecolor('black')
+        patches[i].set_hatch('//')
+
+    for i in range(40, 60):
+        patches[i].set_color(colors[0])
+        patches[i].set_edgecolor('black')
+        patches[i].set_hatch('*')
+
+    font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 12,
+        }
+
+    circ1 = mpatches.Patch( facecolor=colors[1],hatch='//',label='Guest Names')
+    circ2= mpatches.Patch( facecolor=colors[0],hatch='*',label='Topic Names')
+    plt.legend(handles = [circ1,circ2], frameon=False)
+
+    plt.ylabel("Unique Name Count", fontdict=font)
+    plt.xlabel("Number of occurrences in the corpus", fontdict=font)
+    plt.title("Distribution of Unique Name Occurrences", fontdict=font)
+
+    plt.savefig("./plots/hist.svg")
+    plt.show()
+    plt.draw()
+
+def get_name_dict():
     names = {}
     for title, description in zip(all_episodes[0::3], all_episodes[1::3]):
         ep = Episode(title, description)
@@ -30,6 +60,19 @@ def label_set():
                 names[name].append(tokenized)
             else:
                 names[name] = [tokenized]
+    
+    return names
+
+
+def label_set():
+    """
+    Lables the the set.
+    """
+    names = get_name_dict()
+    with open('./names.pickle', 'wb') as f:
+        pickle.dump(names, f)
+
+    plot_names_histogram(names)
                 
     for episode_list in names.values():
         n_mentions = len(episode_list)
