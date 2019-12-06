b2fee30c0b9970b8a6aa33ab8cf65a5c01486ae9
Arvid Larsson <arvla626@student.liu.se>
2019-12-06 15:00:09 +0100

    embedding

---
+++ src/embedding.py
@@ -0,0 +1,16 @@
+import sys
+import pickle
+import numpy as np
+
+glove_file_path = sys.argv[1]
+
+embeddings_index = {}
+with open(glove_file_path, "r") as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype="float32")
+        embeddings_index[word] = coefs
+
+with open("./data/embeddings_index.pickle", 'wb') as f:
+    pickle.dump(embeddings_index, f)
