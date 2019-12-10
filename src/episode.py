505252c9ee1507e4e6dae87c4320fab548633d93
Arvid Larsson <arvla626@student.liu.se>
2019-12-10 20:05:13 +0100

    update episode

--- src/episode.py
+++ src/episode.py
@@ -1,17 +1,13 @@
 import regex as re
 import random
 
+
 class Episode:
-    def __init__(self, title, description, guest=False):
+    def __init__(self, title, description, guest=None):
         self.title = title
         self.description = description
+        self.text = title + "\n" + description
         self.guest = guest
-        if re.findall('FOCUSNAMEFOCUS', title):
-            self.text = "FOCUSNAMEFOCUS " + description
-        else:
-            self.text = title + "\n" + description
-
-        self.text = title + '\n' + description
         self.skip = False
 
     def tokenize(self):
@@ -40,37 +36,29 @@ class Episode:
 
         return names
 
-    def window(self, size=5):
-        text = self.title + " " + self.description
-        words = text.split(" ")
-        win = ["NIL" for i in range(size*2+1)]
-        focus_word = re.findall(r"[^\r\n\t\f\v ]*FOCUSNAMEFOCUS[^\r\n\t\f\v ]*", text)
-        try:
-            focus_index = words.index(focus_word[0])
-        except:
-            self.skip = True
-            print("Failed window")
-            print(words)
-            print(focus_word)
-            input()
-            return text
 
-        word_index = focus_index - size
-        for i, word in enumerate(win):
-            if word_index < 0:
-                word_index += 1
-            elif word_index < len(words) - 1:
-                win[i] = words[word_index]
-                word_index +=1
-        return " ".join(win)
+def window(text, size=30):
+    words = text.split(" ")
+    win = ["NIL" for i in range(size * 2 + 1)]
+    for n, word in enumerate(words):
+        focus_word = re.findall(r"FOCUSNAMEFOCUS", word)
+        if focus_word:
+            focus_index = n
+            break
+    word_index = focus_index - size
 
-        
-
-
+    for i, word in enumerate(win):
+        if word_index < 0:
+            word_index += 1
+        elif word_index < len(words) - 1:
+            win[i] = words[word_index]
+            word_index += 1
+    return " ".join(win)
 
 def get_labeled(data_set, require_blance=False):
     episodes_T = []
     episodes_G = []
+    episodes_unlabeled = []
     lines = data_set.splitlines()
 
     for title, description, label in zip(lines[0::4], lines[1::4], lines[2::4]):
@@ -79,11 +67,17 @@ def get_labeled(data_set, require_blance=False):
             if ep.skip:
                 continue
             episodes_T.append(ep)
+
         elif label == "G":
             ep = Episode(title, description, 1)
             if ep.skip:
                 continue
             episodes_G.append(ep)
+
+        elif label == "_":
+            ep = Episode(title, description)
+            episodes_unlabeled.append(ep)
+
         else:
             print("Wrong label!")
 
@@ -95,12 +89,10 @@ def get_labeled(data_set, require_blance=False):
         else:
             episodes_T = episodes_T[:len_G]
 
-    episodes = []
-    episodes.extend(episodes_G)
-    episodes.extend(episodes_T)
-    random.shuffle(episodes)
+    labeled_episodes = episodes_G + episodes_T
+    random.shuffle(labeled_episodes)
 
-    return episodes
+    return labeled_episodes, episodes_unlabeled
 
 
 def get_unlabeled(data_set):
@@ -112,4 +104,3 @@ def get_unlabeled(data_set):
         episodes.append(ep)
 
     return episodes
-
