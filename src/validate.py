86bc08505fdbf2cee19058b92f912bdaa346ebde
Arvid Larsson <arvla626@student.liu.se>
2019-12-10 20:10:44 +0100

    imporoved matrix

--- src/validate.py
+++ src/validate.py
@@ -7,29 +7,45 @@ from episode import get_labeled
 import sys
 from datetime import date
 
+
+def to_percent(n, n_total):
+    return str(round(n / n_total * 100, 3)) + "%"
+
+
 def log_results(y_predicted, y_true, comment=""):
 
-    def to_percent(n):
-        return str(round(n/len(y_true)*100, 3))+"%"
-
-
     c = metrics.confusion_matrix(y_true, y_predicted)
-    c = [["", "actual T", "actual G", ""],
-        ["predicted T", c[0][0], c[0][1], c[0][0]+ c[0][1]],
-        ["predicted G", c[1][0], c[1][1],c[1][0]+ c[1][1]],
-        ["", c[1][0] + c[0][0], c[1][1] + c[0][1], ""]]
+    sum_pt = c[0][0] + c[0][1]
+    sum_pg = c[1][0] + c[1][1]
+    c = [
+        ["", "actual T", "actual G", ""],
+        [
+            "predicted T",
+            to_percent(c[0][0], sum_pt),
+            to_percent(c[0][1], sum_pt),
+            sum_pt,
+        ],
+        [
+            "predicted G",
+            to_percent(c[1][0], sum_pg),
+            to_percent(c[1][1], sum_pg),
+            sum_pg,
+        ],
+        ["", c[1][0] + c[0][0], c[1][1] + c[0][1], ""],
+    ]
 
     results = ""
     results += str(date.today()) + ":\n"
     results += comment + "\n\n"
-    results += metrics.classification_report(y_true, y_predicted) +"\n"
+    results += metrics.classification_report(y_true, y_predicted) + "\n"
     results += "Confusion Matrix n=" + str(len(y_true)) + ":\n"
     results += tabulate(c) + "\n"
 
     print(results)
     with open("./log.txt", "a+") as f:
         f.write(results)
-        
+
+
 def validate(model, test_set, corpus, comment="Baseline"):
     episodes = get_labeled(test_set, True)
     corpus_test = [ep.text for ep in episodes]
@@ -42,6 +58,7 @@ def validate(model, test_set, corpus, comment="Baseline"):
     y_predicted = model.predict(X)
 
     log_results(y_predicted, y_true, comment)
+
 
 if __name__ == "__main__":
 
